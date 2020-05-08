4de1049172eb47fcc73631de492cf4aae3370f85
first last <prashant.sharma.cse16@itbhu.ac.in>
2020-05-08 21:04:57 +0530

    display ready with button but with no function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import './index.css';
+import { directive } from "@babel/types";
+
+class Square extends React.Component{
+    render(){
+        return(
+            
+           <button className="square" onClick={function() { alert('click');}}>{this.props.value}</button>
+            
+        );
+    }
+}
+class Board extends React.Component {
+    
+    renderSquare(i)
+    {
+       return  <Square value={i} />
+    }
+    render(){
+        const status = 'Next player: X';
+        return (
+
+            <div>
+                <div className="status">
+                    {status}
+                </div>
+
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+
+            </div>
+
+            
+
+
+
+        );
+    }
+}
+
+ class Game extends React.Component{
+     render(){
+         return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+
+            </div>
+         );
+     }
+ }
+  
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
